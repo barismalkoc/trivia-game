d64fc7b675dcbc717f59b447a72076ecf37f74f3
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2021-09-19 15:05:33 +0300

    game is finished

---
+++ index.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btn");
+const numberOfQuestionBtn = document.querySelector("#trivia_amount");
+const selectCategoryBtn = document.querySelector("#trivia_category");
+
+const gameStart = () => {
+
+}
+
+const pullInformation = () => {
+
+
+}
